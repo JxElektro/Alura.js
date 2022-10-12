d371f993ec589f20a28cd9443dc1c36104b412e6
Jhen Nunez <jxelektro@gmail.com>
2022-10-11 22:40:27 -0300

    _4

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+// Creamos una clase cliente
+
 class client {
   name;
   dni;
   rut;
 }
 
+
+// creamos una clase cuenta
 class cuentaCorriente {
   client;
-  balance;
+  #alance;
   number;
-
+  // creamos un constructor que mantine el saldo inicial en 0
   constructor() {
     this.balance = 0;
   }
+  // creamos un metodo para agregar dinero que impide que se agreguen cantidades negativas
   deposit(amount) {
     console.log("Depositando " + amount + " en la cuenta de " + this.client.name);
     if (amount > 0) {
@@ -20,7 +25,7 @@ class cuentaCorriente {
       console.log("No se puede depositar una cantidad negativa");
     }
   }
-
+// creamos un metodo para retirar dinero que impide que se retiren cantidades negativas y que no se pueda retirar mas dinero del que se tiene
   withdraw(amount) {
     console.log("Retirando " + amount);
     if (this.balance >= amount) {
@@ -31,12 +36,13 @@ class cuentaCorriente {
   }
 
 }
-
+// creamos una instancia de la clase cliente
 const client1 = new client();
 client1.name = "Juan";
 client1.dni = 12345678;
 client1.rut = 12345678;
 
+// creamos una instancia de la clase cuenta
 const cuentaCorriente1 = new cuentaCorriente();
 cuentaCorriente1.client = client1;
 cuentaCorriente1.balance = 1000;
@@ -56,6 +62,8 @@ cuentaCorriente2.client = client2;
 cuentaCorriente2.balance = 1000;
 cuentaCorriente2.number = 87654321;
 
-
 cuentaCorriente1.deposit(1000);
 //cuentaCorriente2.deposit(1000);
+
+
+console.log(cuentaCorriente1);
